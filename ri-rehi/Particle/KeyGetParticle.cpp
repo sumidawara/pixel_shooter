123de6bea977ae51a04d15042ca0bdb516cf7a78
sumidawara <sumidawara6532@gmail.com>
2024-11-11 01:58:54 +0900

    KeyParticleの修正

--- ri-rehi/Particle/KeyGetParticle.cpp
+++ ri-rehi/Particle/KeyGetParticle.cpp
@@ -15,6 +15,12 @@ struct KeyGetParticle::Impl
 	Vec2 _start_relative_pos = {0, 0};
 	Vec2 _end_relative_pos = {0, -90};
 
+	Vec2 _key_pos;
+	Vec2 _key_relative_pos = {-25, 0};
+
+	Vec2 _text_pos;
+	Vec2 _text_relative_pos = {35, 0};
+
 	//sparkle
 	double _inner_circle_range = 30;
 	double _outer_circle_range = 60;
@@ -28,8 +34,8 @@ struct KeyGetParticle::Impl
 	{
 		double random_distance = Random(_inner_circle_range, _outer_circle_range);
 		double angle = Random(0.0, 2 * Math::Pi);
-		double x = _pos.x + random_distance * Math::Cos(angle);
-		double y = _pos.y + random_distance * Math::Sin(angle);
+		double x = _key_pos.x + random_distance * Math::Cos(angle);
+		double y = _key_pos.y + random_distance * Math::Sin(angle);
 
 		//キラキラパーティクル
 		auto ptr_sparkle_particle = std::make_shared<SparkleParticle>();
@@ -47,6 +53,9 @@ void KeyGetParticle::init(Vec2 pos)
 	p_impl->_pos = pos;
 	p_impl->_start_pos = pos + p_impl->_start_relative_pos;
 	p_impl->_end_pos = pos + p_impl->_end_relative_pos;
+
+	p_impl->_key_pos = pos + p_impl->_key_relative_pos;
+	p_impl->_text_pos = pos + p_impl->_text_relative_pos;
 }
 
 bool KeyGetParticle::update(double delta_time, double accumulated_time)
@@ -55,7 +64,13 @@ bool KeyGetParticle::update(double delta_time, double accumulated_time)
 
 	auto t = accumulated_time / p_impl->_life_time;
 	auto e = EaseInOut(Easing::Quad, t);
-	p_impl->_pos = MathEx::lerp(p_impl->_start_pos, p_impl->_end_pos, e);
+
+	//座標更新
+	auto pos = MathEx::lerp(p_impl->_start_pos, p_impl->_end_pos, e);
+	p_impl->_pos = pos;
+	p_impl->_key_pos = pos + p_impl->_key_relative_pos;
+	p_impl->_text_pos = pos + p_impl->_text_relative_pos;
+
 	p_impl->generateSparkle();
 
 	return (accumulated_time < p_impl->_life_time);
@@ -63,11 +78,13 @@ bool KeyGetParticle::update(double delta_time, double accumulated_time)
 
 void KeyGetParticle::draw() const
 {
-	auto rect = TextureAsset(AssetKey::key).drawAt(p_impl->_pos);
+	auto key_rect = TextureAsset(AssetKey::key).drawAt(p_impl->_key_pos);
+	auto text_rect = FontAsset(AssetKey::pixel_b30)(U"GET").drawAt(p_impl->_text_pos, GraphicSetting::getYELLOW());
 
 	if(DebugSetting::getIsParticleRectVisible())
 	{
-		rect.draw(DebugSetting::getParticleColor());
+		key_rect.draw(DebugSetting::getParticleColor());
+		text_rect.draw(DebugSetting::getParticleColor());
 	}
 }
 
