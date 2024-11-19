7bc1884d2cd37ca63c137799f164fdeeba46efa1
sumidawara <sumidawara6532@gmail.com>
2024-11-19 18:45:50 +0900

    GameClearSceneは無事にマージできたのだろうか

--- ri-rehi/GameClearScene/GameClearScene.h
+++ ri-rehi/GameClearScene/GameClearScene.h
@@ -10,7 +10,7 @@ public:
     void update() override;
     void draw() const override;
     void drawFadeIn(double t) const override;
-    void drawFadeOut(double t) const override;
+    void drawFadeOut(double t) const override; 
 
 private:
     struct Impl;
